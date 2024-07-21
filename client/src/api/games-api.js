9972378cdc45aed0138c5faf8c7cc70a27cb1748
kevinmunev <kevinmunev@gmail.com>
2024-07-21 15:48:09 +0100

    Add data in practice server

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -2,4 +2,9 @@ import * as request from "./requester";
 
 const BASE_URL = "http://localhost:3030/jsonstore/data/games";
 
-export const getAll = () => request.get(BASE_URL);
+export const getAll = async () => {
+  const response = await request.get(BASE_URL);
+  const games = Object.values(response);
+
+  return games;
+};
