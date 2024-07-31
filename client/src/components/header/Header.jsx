f6df2a8ad401731695d61ccd2f3948d6166f01e1
kevinmunev <kevinmunev@gmail.com>
2024-07-31 15:07:25 +0100

    UI Logged and Guest

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,33 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Header() {
+    const { isAuthenticated } = useContext(AuthContext);
     return (
-        
         <header>
             {/* <!-- Navigation --> */}
-            <h1><Link className="home" to="/">GamesPlay</Link></h1>
+            <h1>
+                <Link className="home" to="/">
+                    GamesPlay
+                </Link>
+            </h1>
             <nav>
                 <Link to="/games">All games</Link>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
+                {isAuthenticated ? (
+                    <div id="user">
+                        <Link to="/games/create">Create Game</Link>
+                        <Link to="/logout">Logout</Link>
+                    </div>
+                ) : (
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                )}
+
                 {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
             </nav>
         </header>
-    )
+    );
 }
